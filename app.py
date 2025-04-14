baacb48367b61fe2418a26c246a92afbd60ed331
Raghavendra-J-R <jrragh@gmail.com>
2025-04-15 05:21:54 +0530

    Changes added

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Sample Jenkins Multi-Stage Pipeline!"
+    return "Hello, Sample Jenkins Multi-Stage Pipeline Added !!"
 
 
 if __name__ == "__main__":
