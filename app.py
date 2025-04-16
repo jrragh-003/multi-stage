7d17b93dd3ca271040b275d93f1a18f3cffe4144
Raghavendra-J-R <jrragh@gmail.com>
2025-04-16 08:11:35 +0530

    modified

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    print("Hello")
+    print("ABC")
+    print("XYZ")
     return "Hello, Sample Jenkins Multi-Stage Pipeline Integrated and few changes yet to be added"
 
 
